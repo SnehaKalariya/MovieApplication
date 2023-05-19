6eff6a081ea295516fc040136eaa97fdf25f3955
sneha.jarsania <sneha.jarsania@synechron.com>
2023-05-19 18:12:33 +0530

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by sneha.jarsania on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
